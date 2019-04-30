021384ef6d36bf30c00e3405c41f11c0e14d7b52
ttom921 <ttom921@hotmail.com>
2019-04-30 08:52:51 +0800

    加入testflasksql

---
+++ Testflasksql/common/ma.py
@@ -0,0 +1,3 @@
+# common/ma.py
+from flask_marshmallow import Marshmallow
+ma = Marshmallow()
---
+++ Testflasksql/crud_user.py
@@ -0,0 +1,97 @@
+from flask import Flask, request, redirect, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+# 在此加入
+from flask_marshmallow import Marshmallow
+
+app = Flask(__name__)
+
+# 資料庫相關
+app.config['SECRET_KEY'] = 'Fianna'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:12345678@localhost:3306/userdb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+ma = Marshmallow(app)
+
+# 定義ORM物件
+
+
+class User(db.Model):
+    __tablename__ = 'tbl_user'
+    user_id = db.Column(db.Integer, nullable=False,
+                        primary_key=True, autoincrement=True)
+    user_name = db.Column(db.String(45), nullable=True)
+    user_email = db.Column(db.String(45), nullable=True)
+    user_password = db.Column(db.String(45), nullable=True)
+
+    def __repr__(self):
+        return '<User %r,user_id %r>' % (self.user_name, self.user_id)
+
+
+class UserSchema(ma.Schema):
+    class Meta:
+        # Fields to expose
+        fields = ('user_id', 'user_name', 'user_email', 'user_password')
+
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+
+# endpoint to show all users
+@app.route("/user", methods=["GET"])
+def get_user():
+    try:
+        all_users = User.query.all()
+        result = users_schema.dump(all_users)
+        return jsonify(result.data)
+    except Exception as e:
+            print("Failed to get all user")
+            print(e)    
+# endpoint to create new user
+@app.route("/user", methods=["POST"])
+def add_user():
+    try:
+        user_name = request.json['user_name']
+        email = request.json['user_email']
+    
+        new_user = User(user_name, email)
+
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify(new_user)   
+    except Exception as e:
+        print("Failed to add user")
+        print(e)    
+
+# endpoint to get user detail  by id
+@app.route("/user/<id>",methods=["GET"])
+def user_detail(id):
+    try:
+        user = User.query.get(id)
+        user_name = request.json['user_name']
+        email= request.json['user_email']
+        user.email=email
+        user.user_name=user_name
+        db.session.commit()
+        return user_schema.jsonify(user)
+    except Exception as e:
+        print("Failed to get user")
+        print(e) 
+# endpoint to delete user
+@app.route("/user/<id>", methods=["DELETE"])
+def user_delete(id):
+    try:
+        user = User.query.get(id)
+        db.session.delete(user)
+        db.session.commit()
+    except:
+        print("Failed to del user")
+        print(e) 
+    
+
+    return user_schema.jsonify(user)        
+	
+if __name__ == "__main__":
+    app.run(debug=False)
