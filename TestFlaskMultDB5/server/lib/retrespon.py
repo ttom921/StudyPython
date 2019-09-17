2a6d20d3ff3052fc1abe1dc57154b54a22e25c6d
ttom921 <ttom921@hotmail.com>
2019-09-17 12:20:06 +0800

    加入lib

---
+++ TestFlaskMultDB5/server/lib/retrespon.py
@@ -0,0 +1,15 @@
+# 建立網頁回覆的json
+def resData(status, msg, tokendata=None):
+    if tokendata:
+        responseObject = {
+            "status": status,
+            "message": msg,
+            "auth_token": tokendata
+        }
+        return responseObject
+    else:
+        responseObject = {
+            "status": status,
+            "message": msg,
+        }
+        return responseObject
