a04e30f7726bfb7fc1cd57fa75216f7f87cc26f5
ttom921 <ttom921@hotmail.com>
2020-01-14 17:38:14 +0800

    更新可執行的程式

--- TestSSE/server/sse.py
+++ TestSSE/server/sse.py
@@ -1,8 +1,14 @@
+import os
 from flask import Flask, render_template
 from flask_sse import sse
+from flask_redis import FlaskRedis
 
 app = Flask(__name__)
-app.config["REDIS_URL"] = "redis://localhost"
+#app.config["REDIS_URL"] = "redis://localhost"
+app.config["REDIS_URL"] = "redis://localhost:6379/0"
+redis_client = FlaskRedis(app)
+print("redis config")
+print(app.config["REDIS_URL"])
 app.register_blueprint(sse, url_prefix='/stream')
 
 @app.route('/')
